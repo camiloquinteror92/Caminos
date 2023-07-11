4c11d624e4ec499ed0fdc67195e38fe3db31f387
camiloquinteror92 <114008160+camiloquinteror92@users.noreply.github.com>
2023-07-11 16:00:29 -0500

    buscador claves

--- InterpreterV2.py
+++ InterpreterV2.py
@@ -15,6 +15,36 @@ def get_layer_records(url, token, where_clause):
 
     return data.get('features')
 
+def search_globalids(search_str, layer_id=0):
+    url = f'{base_url}/{layer_id}/query'
+    # Lista de los campos en los que quieres buscar
+    search_fields = [
+        "organizacion",
+        "subOrganizacion",
+        "nombreDepartamento",
+        "codigoDepartamento",
+        "nombreMunicipio",
+        "codigoMunicipio",
+        "nombreVereda",
+        "codigoVereda",
+        "nombreVeredaLibre",
+        "codigoVia",
+        "nombreVia",
+        "nombreRecolector",
+        "direccionTerritorial",
+        "cargoRecolector",
+        "nombreDelegado",
+    ]
+    where_clause = " OR ".join(f"UPPER({field}) LIKE UPPER('%{search_str}%')" for field in search_fields)
+
+    layer_records = get_layer_records(url, token, where_clause)
+    globalids = [record['attributes']['globalid'] for record in layer_records]
+
+    return globalids
+
+
+    return globalids
+
 def create_html(records, parentglobalid, token):
     if records:
         filename = f'resultados_{parentglobalid}.html'
@@ -26,7 +56,6 @@ def create_html(records, parentglobalid, token):
             table_data = []
             attachments = []
 
-            # Get field aliases for the layer
             field_aliases_url = f"{base_url}/{layer_id}?f=json&token={token}"
             field_aliases_data = requests.get(field_aliases_url).json()
             field_aliases = {field['name']: field['alias'] for field in field_aliases_data['fields']}
@@ -34,7 +63,7 @@ def create_html(records, parentglobalid, token):
             for record in layer_records:
                 attributes = record.get('attributes')
                 for key, value in attributes.items():
-                    field_alias = field_aliases.get(key, key)  # Use alias if available, otherwise use key
+                    field_alias = field_aliases.get(key, key)
                     table_data.append([field_alias, str(value)])
 
                 attachment_url = f"{base_url}/{layer_id}/{record['attributes']['objectid']}/attachments"
@@ -58,9 +87,7 @@ def create_html(records, parentglobalid, token):
     else:
         print(f'No se encontraron registros para parentglobalid: {parentglobalid}')
 
-
-
-
+# Insertar tu código de configuración aquí
 base_url = 'https://services6.arcgis.com/kyerLIHvrND0OSya/arcgis/rest/services/service_050fd5776aea42baa2d4c8fdf1b65aeb/FeatureServer'
 username = 'SINC_Campo_INVIAS'
 password = 'SINC_Campo2022'
@@ -77,17 +104,28 @@ token_response = requests.post(token_url, data=token_params)
 token = token_response.json().get('token')
 
 if token:
-    parentglobalid = input('Ingrese el parentglobalid: ')
-    records = {}
+    search_str = input('Ingrese el departamento, municipio o cualquier otra clave para buscar: ')
+    globalids = search_globalids(search_str)
 
-    for layer_id in range(18):
-        url = f'{base_url}/{layer_id}/query'
-        where_clause = f"globalid = '{parentglobalid}'" if layer_id == 0 else f"parentglobalid = '{parentglobalid}'"
+    if globalids:
+        print(f'Se encontraron {len(globalids)} coincidencias:')
+        for i, globalid in enumerate(globalids, 1):
+            print(f'  {i}. {globalid}')
 
-        layer_records = get_layer_records(url, token, where_clause)
-        if layer_records:
-            records[layer_id] = layer_records
+        print('Generando HTML...')
+        for globalid in globalids:
+            records = {}
 
-    create_html(records, parentglobalid, token)
+            for layer_id in range(18):
+                url = f'{base_url}/{layer_id}/query'
+                where_clause = f"globalid = '{globalid}'" if layer_id == 0 else f"parentglobalid = '{globalid}'"
+
+                layer_records = get_layer_records(url, token, where_clause)
+                if layer_records:
+                    records[layer_id] = layer_records
+
+            create_html(records, globalid, token)
+    else:
+        print(f'No se encontraron coincidencias para: {search_str}')
 else:
     print('Error al generar el token.')
